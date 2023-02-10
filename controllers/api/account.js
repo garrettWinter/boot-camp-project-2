facf415b2be20b5ef3969ecde645d2ddca68c0db
Sal8298 <salahuddinasim95@gmail.com>
2023-02-10 13:41:46 -0600

    developed and fleshed out auth routes. Customer Signup, Login and Logout.

---
+++ controllers/api/account.js
@@ -0,0 +1,71 @@
+const router = require('express').Router();
+const { Customer } = require('../../models');
+
+// CREATE new user
+router.post('/signup', async (req, res) => {
+  console.log(req.body);
+  try {
+    const dbCustomerData = await Customer.create({
+      user_name: req.body.user_name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      req.session.customer_id = dbCustomerData.customer_id;
+      res.status(200).json(dbCustomerData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+
+
+
+router.post('/login', async (req, res) => {
+  try {
+    const CustomerData = await Customer.findOne({ where: { email: req.body.email } });
+    console.log(req.body);
+
+    if (!CustomerData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again --email' });
+      return;
+    }
+
+    const validPassword = await CustomerData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again --password' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.customer_id = CustomerData.customer_id;
+      req.session.logged_in = true;
+      
+      res.json({ Customer: CustomerData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
--- controllers/homepageRoutes.js
+++ controllers/homepageRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Customer, LineItem, Product, Order } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {

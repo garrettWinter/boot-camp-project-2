79146ead1621c193ac06757b4f675d6ef4730433
Sal8298 <salahuddinasim95@gmail.com>
2023-02-13 12:48:37 -0600

    Fleshed out accountRoutes and started cartRoutes.

--- controllers/cartRoutes.js
+++ controllers/cartRoutes.js
@@ -2,16 +2,22 @@ const router = require('express').Router();
 const { Customer, LineItem, Product, Order } = require('../models');
 
 router.get('/', async (req, res) => {
-
+  const cart = JSON.parse(localStorage.getItem('cart'));
+  const products = [];
   
+  for (let i = 0; i < cart.length; i++) {
+   products.push(cart[i].product_id);
+   console.log(products);
+  }
+
   try {
     const dbProductData = await Product.findAll({});
-
-    const homepage = dbProductData.map((product) =>
-      product.get({ plain: true })
+    const cart = dbProductData.map((product) =>
+    product.get({ plain: true })
     );
 
     res.render('cart', {
+      cart,
       logged_in: req.session.logged_in,
     });
 

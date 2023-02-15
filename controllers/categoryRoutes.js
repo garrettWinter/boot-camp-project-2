388898f54b7cf2991cd30f86f4f8ea92b9de96aa
NicholasHM05 <nhmamberger@gmail.com>
2023-02-14 23:10:31 -0600

    added buttons that link to the other pages, fixed product page, fixed many handlebars.

--- controllers/categoryRoutes.js
+++ controllers/categoryRoutes.js
@@ -1,29 +1,51 @@
+// const router = require('express').Router();
+// const { Customer, LineItem, Product, Order } = require('../models');
+// const { Op } = require('sequelize');
+
+// router.get('/:term', async (req, res) => {
+//   console.log("Inside Search Controller") // Can see this in the terminal logs
+//   searchTerm = ("'%"+req.params.term+"%'");
+//   console.log(searchTerm); // Can see this in the terminal logs
+//   try {
+//     const dbSearchData = await Product.findAll({
+//       where: {
+//         product_genre:  {
+//           [Op.like]: `%${req.params.term}%`
+//         }
+//       },
+//     });
+//     console.log("Completed the find");
+//     console.log(dbSearchData);
+//     const results = dbSearchData.map((results) =>
+//       results.get({ plain: true })
+//     );
+
+//   console.log("Completed the map");
+//    console.log(results); // CANNOT SEE THIS IN THE TERMINAL... UNSURE IF THE FINDALL IS WORKING
+//     res.render('category', {
+//       results,
+//       logged_in: req.session.logged_in,
+//     });
+
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// }
+// )
+
+// module.exports = router;
 const router = require('express').Router();
 const { Customer, LineItem, Product, Order } = require('../models');
-const { Op } = require('sequelize');
 
-router.get('/:term', async (req, res) => {
-  console.log("Inside Search Controller") // Can see this in the terminal logs
-  searchTerm = ("'%"+req.params.term+"%'");
-  console.log(searchTerm); // Can see this in the terminal logs
+router.get('/', async (req, res) => {
   try {
-    const dbSearchData = await Product.findAll({
-      where: {
-        product_genre:  {
-          [Op.like]: `%${req.params.term}%`
-        }
-      },
-    });
-    console.log("Completed the find");
-    console.log(dbSearchData);
-    const results = dbSearchData.map((results) =>
-      results.get({ plain: true })
+    const dbProductData = await Product.findAll({});
+
+    const homepage = dbProductData.map((product) =>
+      product.get({ plain: true })
     );
 
-  console.log("Completed the map");
-   console.log(results); // CANNOT SEE THIS IN THE TERMINAL... UNSURE IF THE FINDALL IS WORKING
     res.render('category', {
-      results,
       logged_in: req.session.logged_in,
     });
 
@@ -32,5 +54,4 @@ router.get('/:term', async (req, res) => {
   }
 }
 )
-
 module.exports = router;
--- controllers/productRoutes.js
+++ controllers/productRoutes.js
@@ -6,9 +6,10 @@ router.get('/:id', async (req, res) => {
   try {
     const dbProductData = await Product.findByPk(req.params.id);
 
-    const productDetail = dbProductData.get({ plain: true });
+    const productDetail = [dbProductData.get({ plain: true })];
 
     console.log(productDetail);
+    console.log(dbProductData);
     res.render('product', {
       productDetail,
       logged_in: req.session.logged_in,
@@ -20,5 +21,39 @@ router.get('/:id', async (req, res) => {
   }
 }
 )
+// Creating the post page
+// router.post("/", async(req, res) => {
+//   try {
+//       const newPost = await productDetail.create({
+//           ...req.body,
+//           user_id: req.session.user_id,
+//       });
+
+//       res.status(200).json(newPost);
+//   } catch (err) {
+//       res.status(400).json(err);
+//   }
+// });
+// router.get('/:id', async (req, res) => {
+//   try {
+//     const dbProductData = await Product.findAll({
+//       limit: 1,
+//     });
+//     const product = dbProductData.map((product) =>
+//       product.get({ plain: true })
+//     );
+
+//     console.log(product);
+//     res.render('product', {
+//       product,
+//       logged_in: req.session.logged_in,
+//     });
+  
+
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// }
+// )
 
 module.exports = router;

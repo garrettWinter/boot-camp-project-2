eda900af25c8eec482fa20f74eefb7d722095732
Garrett Winter <Garrett_Winter@hotmail.com>
2023-02-15 14:32:33 -0600

    Got the submit cart functionalty

--- public/js/cartSubmit.js
+++ public/js/cartSubmit.js
@@ -1,15 +1,21 @@
 console.log("cartSubmit.js is connected");
 
-const submitOrderBtn = document.querySelector('#submitOrderBtn'); /// UPdate to point to button ID when created 
-
-async function cartSubmit() {
+const cartSubmit = async () => {
     console.log('Cart Submit triggered')
-
     const response = await fetch('/api/submitorder/createorder', { // This path needs to be updated!
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
     });
-}
+    console.log(response);
+    if (response.ok) {
+        console.log('in response')
+        document.location.replace('/account/orderhistory');
+    } else {
+        alert('Failed to submit cart.');
+    }
+
+};
 
 
+const submitOrderBtn = document.querySelector('#submitOrderBtn'); /// UPdate to point to button ID when created 
 submitOrderBtn.addEventListener("click", cartSubmit)

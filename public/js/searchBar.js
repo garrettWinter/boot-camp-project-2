aedb0e620b1e564cb25cabaa6fe05084d23058e7
Garrett Winter <Garrett_Winter@hotmail.com>
2023-02-15 22:06:53 -0600

    Added error handling on search bar if the users attempts to search on nothing.

--- public/js/searchBar.js
+++ public/js/searchBar.js
@@ -6,14 +6,15 @@ const searchBtn = document.querySelector('#searchBtn');
 
 
 const searchRedirect = async (event) => {
+  console.log(searchBar.value);
+  if (searchBar.value == '' ){
+    window.alert("Please enter a part of a movie name in the search bar, and search again.")
+  }
   event.preventDefault();
   const term = searchBar.value.trim();
   console.log(term);
-  location.replace('/search/' + term);
-
-
+  // location.replace('/search/' + term);
 };
 
 //no idea why but submit has issues eventually. click is most reliable
 searchBtn.addEventListener('click', searchRedirect);
-console.log(searchRedirect)

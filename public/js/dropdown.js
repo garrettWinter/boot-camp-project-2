9a81e2c5d207df9dd4a1adad39190cd1ded243cb
Garrett Winter <Garrett_Winter@hotmail.com>
2023-02-15 21:05:33 -0600

    Updates for the dropdown button on the genre page.

--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -1,4 +1,6 @@
 var dropdown = document.querySelector('.dropdown')
 dropdown.addEventListener('click', function(){
-    dropdown.setAttribute('class', 'is-hoverable')
-})
+    dropdown.setAttribute('class', 'is-active');
+});
+
+dropdown.addEventListener('focusout', () => dropdown.setAttribute('class', 'dropdown is-hoverable'));
